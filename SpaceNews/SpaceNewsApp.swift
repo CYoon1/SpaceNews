fb269f58674405f4934ca07e567665c1bf46e0d3
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2023-03-16 10:06:49 -0400

    Initial Commit

---
+++ SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by Christopher Yoon on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
